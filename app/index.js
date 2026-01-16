8cefe194329eeeb2e5081fe135fd0349c5a4426c
DESKTOP-0CI21OO\user1 <hello>
2026-01-16 12:02:05 +0900

    js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,28 @@
-console.log("zzzz");
+function handleJoin() {
+  const userId = document.querySelector("input[name=userId]").value.trim();
+  const userPw = document.querySelector("input[name=userPw]").value.trim();
+  const userPw2 = document.querySelector("input[name=userPw2]").value.trim();
+  const userNick = document.querySelector("input[name=userNick]").value.trim();
+
+  //id(영어 숫자 허용, 4~8글자)
+  const idOK = /^[a-z0-9]{4,8}$/.test(userId);
+  if (!idOK) {
+    alert("아이디 확인바람");
+    return false;
+  }
+  //pw
+  const pwOK1 = /^[0-9]{4,8}$/.test(userPw);
+  const pwOK2 = userPw === userPw2;
+  const pwOK = pwOK1 && pwOK2;
+  if (!pwOK) {
+    alert("비밀번호 확인바람");
+    return false;
+  }
+
+  //nick
+  const nickOK = /^[가-힣]{2,4}$/.test(userNick);
+  if (!nickOK) {
+    alert("닉네임 확인바람");
+    return false;
+  }
+}
